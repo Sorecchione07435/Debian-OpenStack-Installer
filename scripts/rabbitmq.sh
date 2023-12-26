ebd297d440d3f6d28c164af3a568ee8e2a817c43
Sorecchione07435 <111366201+Sorecchione07435@users.noreply.github.com>
2023-12-26 10:20:59 +0100

    Update rabbitmq.sh

--- scripts/rabbitmq.sh
+++ scripts/rabbitmq.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
 set -o xtrace
-
 source openstack.conf
 
 add_openstack_user(){
-    rabbitmqctl add_user openstack $RABBITMQ_PASSWORD
+
+rabbitmqctl add_user openstack $RABBITMQ_PASSWORD
 	
 rabbitmqctl set_permissions openstack ".*" ".*" ".*"
 
